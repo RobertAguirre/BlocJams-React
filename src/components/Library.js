edb0eea664ab08f19c87c7e416f9cde24940a3ea
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2018-02-07 16:46:32 -0500

    library component state and replication of an API response to render albums

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import albumData from '../data/album.js';
 
 class Library extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {albums: albumData};
+  }
   render() {
     return (
       <section className="library">
-        Soon to be the home of the BlocJam's Library!
+        {this.state.albums.map((album, index) =>
+          <div key={index}>
+            <img src={album.albumCover} alt={album.title} />
+            <div>{album.title}</div>
+            <div>{album.artist}</div>
+            <div>{album.songs.length} songs</div>
+          </div>
+        )}
       </section>
-    )
+    );
   }
 }
-    
+
 export default Library;
