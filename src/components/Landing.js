70bb9ebf1f58c0fb1de92b6de14f5bf7397d94ab
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2018-02-06 23:46:45 -0500

    initial component setup for landing and library

---
+++ src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Landing = () => (
+  <section className="landing">
+    Coming soon - The Landing page for BlocJams!
+  </section>
+);
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Library = () => (
+  <section className="library">
+    Soon to be the home of the BlocJam's Library!
+  </section>
+);
+
+export default Library;
